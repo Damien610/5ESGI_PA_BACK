78c9d3065638124c2173872f8b1cbcbe523f3d54
DamienNerri <d.nerriere@orisha.com>
2025-09-17 15:55:10 +0200

    otp logics

--- tests/test_otp/test_smtp.py
+++ tests/test_otp/test_smtp.py
@@ -5,7 +5,7 @@ from app.utils.otp_service import send_otp_email
 load_dotenv()
 
 # Test d'envoi
-email_test = "votre-email-test@example.com"
+email_test = "damien.nerri@gmail.com"
 otp_test = "123456"
 
 if send_otp_email(email_test, otp_test):
